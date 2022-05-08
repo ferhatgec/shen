1d92b5ccdc5a7787c2664bf5a2754914a62f39ca
ferhatgec <ferhat2gecdogan_fix_me5@protonmail.com>
2022-05-08 06:33:26 +0300

    Add: Source.

---
+++ example.py
@@ -0,0 +1,70 @@
+from shen import *
+
+data = ''
+
+with open('shen.py', 'r') as file:
+    for line in file:
+        data += line
+
+    file.close()
+
+data += '\n'
+
+init = shen()
+rule = shen_syntax_rule()
+rule.capture_through["'"] = shen_syntax_rule.shen_color(10, 20, 10, "'(.*?)'", True, True).return_inside()
+rule.capture_through["#"] = shen_syntax_rule.shen_color(10, 10, 30, '#(.*)\n', '\n', True, True).return_inside()
+
+rule.capture_through["{"] = shen_syntax_rule.shen_color(108, 10, 200, '{(.*)}', '}', True, True).return_inside()
+rule.push_tokens = ['(', ')', ';', ' ', "'", ':', '\n', '[', ']', '.', '{', '}', '"', ',',
+                    '=', '+', '/', '<', '>', '%', '*']
+
+rule.supported_tokens["def"] = shen_syntax_rule.shen_color(100, 20, 30, '', False).return_inside()
+rule.supported_tokens["int"] = shen_syntax_rule.shen_color(100, 20, 200, '', False).return_inside()
+rule.supported_tokens["str"] = shen_syntax_rule.shen_color(100, 20, 200, '', False).return_inside()
+rule.supported_tokens["dict"] = shen_syntax_rule.shen_color(100, 20, 200, '', False).return_inside()
+rule.supported_tokens["from"] = shen_syntax_rule.shen_color(100, 200, 200, '', False).return_inside()
+rule.supported_tokens["import"] = shen_syntax_rule.shen_color(100, 100, 200, '', False).return_inside()
+rule.supported_tokens["class"] = shen_syntax_rule.shen_color(10, 20, 200, '', False).return_inside()
+rule.supported_tokens["self"] = shen_syntax_rule.shen_color(50, 200, 200, '', False).return_inside()
+rule.supported_tokens["str"] = shen_syntax_rule.shen_color(100, 20, 200, '', False).return_inside()
+rule.supported_tokens["bool"] = shen_syntax_rule.shen_color(100, 20, 200, '', False).return_inside()
+rule.supported_tokens["if"] = shen_syntax_rule.shen_color(105, 30, 20, '', False).return_inside()
+rule.supported_tokens["elif"] = shen_syntax_rule.shen_color(105, 30, 20, '', False).return_inside()
+rule.supported_tokens["else"] = shen_syntax_rule.shen_color(105, 30, 20, '', False).return_inside()
+rule.supported_tokens["for"] = shen_syntax_rule.shen_color(105, 30, 20, '', False).return_inside()
+rule.supported_tokens["break"] = shen_syntax_rule.shen_color(125, 55, 25, '', False).return_inside()
+rule.supported_tokens["True"] = shen_syntax_rule.shen_color(100, 200, 31, '', False).return_inside()
+rule.supported_tokens["False"] = shen_syntax_rule.shen_color(200, 20, 69, '', False).return_inside()
+rule.supported_tokens["continue"] = shen_syntax_rule.shen_color(130, 60, 50, '', False).return_inside()
+rule.supported_tokens["match"] = shen_syntax_rule.shen_color(105, 70, 20, '', False).return_inside()
+rule.supported_tokens["case"] = shen_syntax_rule.shen_color(105, 30, 20, '', False).return_inside()
+rule.supported_tokens["not"] = shen_syntax_rule.shen_color(200, 30, 20, '', False).return_inside()
+rule.supported_tokens["is"] = shen_syntax_rule.shen_color(10, 250, 20, '', False).return_inside()
+rule.supported_tokens["in"] = shen_syntax_rule.shen_color(10, 250, 20, '', False).return_inside()
+rule.supported_tokens["not"] = shen_syntax_rule.shen_color(200, 30, 20, '', False).return_inside()
+rule.supported_tokens["and"] = shen_syntax_rule.shen_color(40, 50, 130, '', False).return_inside()
+rule.supported_tokens["or"] = shen_syntax_rule.shen_color(40, 50, 100, '', False).return_inside()
+rule.supported_tokens["return"] = shen_syntax_rule.shen_color(40, 100, 20, '', False).return_inside()
+rule.supported_tokens["None"] = shen_syntax_rule.shen_color(200, 250, 100, '', False).return_inside()
+
+rule.supported_tokens["__init__"] = shen_syntax_rule.shen_color(200, 100, 50, '', False).return_inside()
+rule.supported_tokens["len"] = shen_syntax_rule.shen_color(50, 100, 50, '', False).return_inside()
+
+rule.supported_tokens["->"] = shen_syntax_rule.shen_color(250, 250, 1, '', False).return_inside()
+rule.supported_tokens["("] = shen_syntax_rule.shen_color(100, 100, 100, '', False).return_inside()
+rule.supported_tokens[")"] = shen_syntax_rule.shen_color(100, 100, 100, '', False).return_inside()
+rule.supported_tokens["["] = shen_syntax_rule.shen_color(100, 100, 100, '', False).return_inside()
+rule.supported_tokens["]"] = shen_syntax_rule.shen_color(100, 100, 100, '', False).return_inside()
+rule.supported_tokens["+"] = shen_syntax_rule.shen_color(100, 10, 100, '', False).return_inside()
+rule.supported_tokens["/"] = shen_syntax_rule.shen_color(100, 10, 100, '', False).return_inside()
+rule.supported_tokens["%"] = shen_syntax_rule.shen_color(100, 10, 100, '', False).return_inside()
+rule.supported_tokens["!"] = shen_syntax_rule.shen_color(100, 10, 100, '', False).return_inside()
+rule.supported_tokens["="] = shen_syntax_rule.shen_color(100, 10, 100, '', False).return_inside()
+rule.supported_tokens["-"] = shen_syntax_rule.shen_color(100, 10, 100, '', False).return_inside()
+rule.supported_tokens[">"] = shen_syntax_rule.shen_color(100, 10, 100, '', False).return_inside()
+rule.supported_tokens["<"] = shen_syntax_rule.shen_color(100, 10, 100, '', False).return_inside()
+rule.supported_tokens["="] = shen_syntax_rule.shen_color(100, 10, 100, '', False).return_inside()
+
+for x in (init.highlight(data, rule)):
+    print(x, end='')
---
+++ shen.py
@@ -0,0 +1,125 @@
+# MIT License
+#
+# Copyright (c) 2022 Ferhat Geçdoğan All Rights Reserved.
+# Distributed under the terms of the MIT License.
+#
+# shen - syntax highlighter thing
+# -------------------------------
+# simplest way to highlighting your code as fast and simple as possible.
+#
+# github.com/ferhatgec/shen
+#
+
+from re import search
+
+class shen_syntax_rule:
+    class shen_color:
+        def __init__(self, r, g, b, pattern, ends_with='', enable_regex=False, current_char_coloring=True):
+            self.r = r
+            self.g = g
+            self.b = b
+
+            self.pattern: str = pattern
+            self.ends_with: str = ends_with
+            self.enable_regex = enable_regex
+            self.include_current_char_coloring = current_char_coloring
+
+
+        def return_inside(self):
+            return self
+
+    def __init__(self):
+        self.supported_tokens = dict()
+        self.push_tokens = []
+        self.capture_through = dict()
+        self.ignored_tokens = []
+
+
+class shen:
+    def __init__(self):
+        self.tokens = []
+        self.current_token: str = ''
+        self.__collect = False
+        self.__capture_ch = ''
+
+    def highlight(self, data: str, syntax_rule: shen_syntax_rule) -> [str]:
+        if len(syntax_rule.push_tokens) == 0:
+            return ['push_token[] is empty']
+
+        for ch in data:
+            if (syntax_rule.capture_through.get(ch) is not None) or self.__collect:
+                if self.__collect:
+                    if ch == self.__capture_ch or (ch == syntax_rule.capture_through.get(self.__capture_ch).ends_with):
+                        if len(self.current_token) > 0:
+                            self.current_token += ch
+                            data = syntax_rule.capture_through.get(self.__capture_ch)
+
+                            if data.enable_regex:
+                                val = search(data.pattern, self.current_token)
+
+                                if val is not None:
+                                    val_data = val.groups()
+                                    if len(val_data) >= 1:
+                                        if ch == syntax_rule.capture_through.get(self.__capture_ch).ends_with:
+                                            if data.include_current_char_coloring:
+                                                self.tokens.append(f'\x1b[38;2;{data.r};{data.g};{data.b}m{self.__capture_ch}{val_data[0]}{ch}\x1b[0m')
+                                            else:
+                                                self.tokens.append(f'{self.__capture_ch}\x1b[38;2;{data.r};{data.g};{data.b}m{val_data[0]}\x1b[0m{ch}')
+                                        else:
+                                            if data.include_current_char_coloring:
+                                                self.tokens.append(
+                                                    f'\x1b[38;2;{data.r};{data.g};{data.b}m{ch}{val_data[0]}{ch}\x1b[0m')
+                                            else:
+                                                self.tokens.append(
+                                                    f'{ch}\x1b[38;2;{data.r};{data.g};{data.b}m{val_data[0]}\x1b[0m{ch}')
+                                else:
+                                    self.__collect = False
+                                    self.current_token += ch
+                                    continue
+                            else:
+                                self.tokens.append(self.current_token)
+
+                            self.current_token = ''
+                            self.__collect = False
+                    else:
+                        self.current_token += ch
+                else:
+                    self.__collect = True
+                    self.__capture_ch = ch
+                    self.current_token += ch
+
+                continue
+
+            match ch:
+                case ch if (ch in syntax_rule.push_tokens) or (self.current_token in syntax_rule.push_tokens):
+                    if len(self.current_token) > 0:
+                        data = syntax_rule.supported_tokens.get(self.current_token)
+
+                        if data is not None:
+                            if data.enable_regex:
+                                val = search(data.pattern, self.current_token).groups()
+
+                                if len(val) >= 1:
+                                    self.tokens.append(f'\x1b[38;2;{data.r};{data.g};{data.b}m{val[1]}\x1b[0m')
+                            else:
+
+                                self.tokens.append(f'\x1b[38;2;{data.r};{data.g};{data.b}m{self.current_token}\x1b[0m')
+                        else:
+                            self.tokens.append(self.current_token)
+
+                    self.current_token = ''
+
+                    if ch not in syntax_rule.ignored_tokens and not self.__collect:
+                        data = syntax_rule.supported_tokens.get(ch)
+
+                        if data is not None:
+                            self.tokens.append(f'\x1b[38;2;{data.r};{data.g};{data.b}m{ch}\x1b[0m')
+                        else:
+                            self.tokens.append(ch)
+
+                    continue
+
+                case _:
+                    self.current_token += ch
+
+        return self.tokens
